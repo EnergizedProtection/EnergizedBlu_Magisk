c1269a447da1673290d86498851513ada307854a
Ador <adroit.ador@live.com>
2018-06-04 04:52:24 +0600

    Added Prop Fetcher.

--- fetch.sh
+++ fetch.sh
@@ -3,5 +3,7 @@
 # Fetch the latest Energized Blu Hosts.
 #
 HOSTS_URL=https://raw.githubusercontent.com/EnergizedProtection/EnergizedBlu/master/EnergizedBlu/energized/blu
+PROP_URL=https://raw.githubusercontent.com/EnergizedProtection/EnergizedBlu/master/EnergizedBlu/zip/Magisk/module.prop
  
 wget -O system/etc/hosts $HOSTS_URL
+wget -N $PROP_URL
