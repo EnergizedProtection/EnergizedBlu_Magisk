8bd775167f77c1ab6cf1645b1a3b373993402bac
adroit.ador <adroit.ador@live.com>
2018-06-12 11:34:59 -0400

    Fixed Link and Merged latest hosts.

--- fetch.sh
+++ fetch.sh
@@ -2,8 +2,8 @@
 # Energized Blu Fetch
 # Fetch the latest Energized Blu Hosts.
 #
-HOSTS_URL=https://raw.githubusercontent.com/EnergizedProtection/EnergizedBlu/master/EnergizedBlu/energized/blu
-PROP_URL=https://raw.githubusercontent.com/EnergizedProtection/EnergizedBlu/master/EnergizedBlu/zip/Magisk/module.prop
+HOSTS_URL=https://raw.githubusercontent.com/EnergizedProtection/EnergizedBlu/master/energized/blu
+PROP_URL=https://raw.githubusercontent.com/EnergizedProtection/EnergizedBlu/master/zip/Magisk/module.prop
 README_URL=https://raw.githubusercontent.com/EnergizedProtection/EnergizedBlu/master/README.md
  
 wget -O system/etc/hosts $HOSTS_URL
